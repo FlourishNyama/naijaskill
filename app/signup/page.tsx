ce488b0bb0e7daf4698c83ae21fbf3846d6f9ac8
FlourishNyama <133850266+FlourishNyama@users.noreply.github.com>
2026-02-27 00:55:27 +0100

    try eye icon toggle

--- app/signup/page.tsx
+++ app/signup/page.tsx
@@ -1,6 +1,6 @@
+"use client";
 import { useState } from 'react';
 import { Eye, EyeOff } from 'lucide-react';
-"use client";
 import { useState } from 'react';
 import Link from 'next/link';
 import Image from 'next/image';
